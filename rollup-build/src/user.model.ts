91315736243a3a000863b287360220fb7a25dc65
Chau Tran <ctch5@mail.umsl.edu>
2020-02-04 14:04:30 -0600

    update rollup example

--- rollup-build/src/user.model.ts
+++ rollup-build/src/user.model.ts
@@ -1,8 +1,11 @@
 import { AutoMap } from '@nartc/automapper';
+import {Type, Expose} from 'class-transformer';
+import { Address } from './address.model';
 import { Profile } from './profile.model';
 
 export class User {
   firstName!: string;
   lastName!: string;
   profile!: Profile;
+  addresses!: Address[];
 }
